ec6be50cbe76a2cf319c230a86b5f47a50530346
pmbaldaia <40200388@esmad.ipp.pt>
2021-12-29 18:06:00 +0000

    update

--- Projeto/Coding/calypso/src/views/Homepage.vue
+++ Projeto/Coding/calypso/src/views/Homepage.vue
@@ -32,3 +32,66 @@
     </nav>
   </header>
 </template>
+<style>
+header {
+  z-index: 99;
+  width: 100%;
+  position: fixed;
+  transition: 0.5s ease all;
+  color: #023047;
+}
+
+nav {
+  font-family: "Rubik";
+  color: #023047;
+  display: flex;
+  flex-direction: row;
+  padding: 12px 0;
+  transition: 0.5s ease all;
+  width: 90%;
+  margin: 0 auto;
+}
+
+ul,
+.link {
+  font-weight: 500;
+  color: #023047;
+  list-style: none;
+  text-decoration: none;
+}
+li {
+  text-transform: uppercase;
+  padding: 16px;
+  margin-left: 16px;
+}
+.link {
+  font-size: 14px;
+  transition: 0.5s ease all;
+  padding-bottom: 4px;
+  border-bottom: 1px solid transparent;
+}
+li a:hover {
+  border-bottom: #ac3931 solid 1px;
+}
+
+.branding {
+  display: flex;
+  align-items: center;
+}
+img {
+  width: 50px;
+  transition: 0.5s ease all;
+}
+
+.navigation {
+  display: flex;
+  align-items: center;
+  flex: 1;
+  justify-content: flex-end;
+}
+
+.icon-login {
+  width: 15px;
+  height: 15px;
+}
+</style>
